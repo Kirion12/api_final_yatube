c75ac26a98ead504fdd0f2efea39b8fdd39ef3b7
Дмитрий Титовский <ttvsk.dima@yandex.ru>
2023-02-20 17:49:03 +0300

    update 20.02

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,58 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework.pagination import LimitOffsetPagination
+from posts.models import Group, Post, User
+from rest_framework import filters, permissions, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (
+    CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = LimitOffsetPagination
+    search_fields = ('=author')
+    ordering_fields = ('pub_date', 'author')
+    ordering = ('pub_date',)
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user, post=self.get_post())
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post']
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = FollowSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', 'following__username']
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user.username)
+        return user.follower
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
